0a3b8ff118b830f2d3da83c6062b0f708e9a1e9f
Antoni Matheus <antonimatheus789@gmail.com>
2023-03-04 16:24:53 -0400

    aulas.CeV

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1
+while (c <= 6 ) {
+    console.log(`Passo ${c}`)
+     // c++ é igual a // c = c + 1
+     c++
+}*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+     // c++ é igual a // c = c + 1
+     c++     
+} while (c <= 6 )
