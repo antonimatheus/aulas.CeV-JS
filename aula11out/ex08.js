1468c94b8406aca46bb1209e89068b6e017a5825
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-04-29 14:58:17 -0400

    Exercícios 8,9,10 feitos

--- aula11out/ex08.js
+++ aula11out/ex08.js
@@ -1,1 +1,7 @@
-console.log('O console funcionou corretamente!')
+var vel = window.prompt('Velociade do carro? ')
+if (vel > 60) {
+    console.log(`MULTADO! Você ultrapassou o limite de ${vel}km/h`)
+} else {
+    console.log('Boa viagem!')
+}
+console.log('Dirija sempre com sinto de segurança!')
---
+++ aula11out/ex09.js
@@ -0,0 +1,6 @@
+var pais = 'Rússia'
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é entrangeiro')
+}
