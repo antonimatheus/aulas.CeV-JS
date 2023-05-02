f1206e1c04b6d3ec1a7591f8d69ab9b226f79576
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-05-02 15:12:46 -0400

    +1 aula concluída

---
+++ aula13out/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1 
+do {
+    console.log('Tudo bem?')
+    c++
+}while(c <= 6)
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 4)
+
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+var c = 1
+while (c < 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
