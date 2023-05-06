38f48bc8dceb0ebc2277c862e0c6004b40037edc
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-05-06 14:55:12 -0400

    exercício 4 feito com while

--- exercício04out/index.js
+++ exercício04out/index.js
@@ -1,14 +1,34 @@
-function enviar() {
+/*function enviar() {
     var tabu = window.document.getElementById('itxtt')
     var res = window.document.getElementById('res')
     res.innerHTML = ''
     var tab = Number(tabu.value)
-    if (tab <=0) {
+    if (tabu.value.length == 0 ) {
         res.innerHTML = 'IMPOSSÍVEL gerar uma tabuada, adicione um número!'
     } else {
       for (var i = 1; i <= 10; i++) {
         var resp = tab * i
         res.innerHTML += `${tab} x ${i} = ${resp}<br>`  
     }
+    }
+}*/
+
+function enviar() {
+    var tabu = document.getElementById('itxtt')
+    var res = document.getElementById('seltab')
+    
+    if (tabu.value.length == 0) {
+        window.alert('ERRO! Digite um número!')
+    } else {
+        let tab = Number(tabu.value)
+        var c = 1
+        res.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${tab} x ${c} = ${tab*c}`
+            res.appendChild(item)
+            //item.value = `tabu${c}`
+            c++
+        }
     }
 }
